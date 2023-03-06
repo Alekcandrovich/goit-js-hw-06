f038c843e02a8b247e86a05a1949c108f55d451a
Alekcandrovich <117031030+Alekcandrovich@users.noreply.github.com>
2023-03-06 20:48:09 +0200

    сделаны 9 задач и отправлены на проверку

---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+// Напиши скрипт который,
+// при наборе текста в инпуте input#name - input(событие input),
+// подставляет его текущее значение в span#name - output.
+// Если инпут пустой, в спане должна отображаться строка "Anonymous".
+
+// <input type="text" id="name-input" placeholder="Please enter your name" />
+// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+
+const nameInputRef = document.getElementById('name-input');
+const nameOutputRef = document.getElementById('name-output');
+
+nameInputRef.addEventListener('input', () => {
+  nameOutputRef.textContent = nameInputRef.value || 'Anonymous';
+});
